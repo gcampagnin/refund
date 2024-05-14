ba8230b93e420a53a65c1674a06879d94ace0ef6
Gustavo Campagnin <gcampagnin@gmail.com>
2024-05-14 10:33:30 -0300

    add function expenseAdd

--- scripts.js
+++ scripts.js
@@ -32,4 +32,15 @@ form.onsubmit = (event) => {
 		amount: amount.value,
 		created_at: new Date(),
 	}
+
+	expenseAdd(newExpense)
+}
+
+function expenseAdd(newExpense) {
+	try {
+
+	} catch (error) {
+		alert("Não foi possível adicionar a despesa.")
+		console.log(error)
+	}
 }
