1da303bfe3212d43511fef3fbb644474fa230198
Gustavo Campagnin <gcampagnin@gmail.com>
2024-05-14 09:42:40 -0300

    input amount acept only numbers

--- scripts.js
+++ scripts.js
@@ -1,1 +1,8 @@
-console.log("Conectado")
+// Seleciona os elementos do form.
+const amount = document.getElementById("amount")
+
+amount.oninput = () => {
+	let value = amount.value.replace(/\D/g, "")
+
+	amount.value = value
+}
